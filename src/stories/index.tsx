4cd7930ae4e669c204c7f7145abcefe8d37249ea
Shingo Yamazaki <shingoyamazaki00@gmail.com>
2019-06-24 20:48:04 +0900

    Add Button

--- src/stories/index.tsx
+++ src/stories/index.tsx
@@ -1,19 +1,14 @@
-import React from 'react';
+import React from "react";
 
-import { storiesOf } from '@storybook/react';
-import { action } from '@storybook/addon-actions';
-import { linkTo } from '@storybook/addon-links';
+import { storiesOf } from "@storybook/react";
+import { action } from "@storybook/addon-actions";
 
-import { Button, Welcome } from '@storybook/react/demo';
+import { Button } from "../Button";
 
-storiesOf('Welcome', module).add('to Storybook', () => <Welcome showApp={linkTo('Button')} />);
-
-storiesOf('Button', module)
-  .add('with text', () => <Button onClick={action('clicked')}>Hello Button</Button>)
-  .add('with some emoji', () => (
-    <Button onClick={action('clicked')}>
-      <span role="img" aria-label="so cool">
-        😀 😎 👍 💯
-      </span>
-    </Button>
+storiesOf("Button", module)
+  .add("normal", () => (
+    <Button onClick={action("clicked")} text="Hello Button" />
+  ))
+  .add("submit", () => (
+    <Button onClick={action("clicked")} text="Hello Button" type="submit" />
   ));
